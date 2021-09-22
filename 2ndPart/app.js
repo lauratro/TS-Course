54e27bf048c80ddc58779f17458a8066fad981f5
Laura <65158950+lauratro@users.noreply.github.com>
2021-09-22 17:23:28 +0200

    added second part index in the root

--- 2ndPart/app.js
+++ 2ndPart/app.js
@@ -1,1 +1,17 @@
-console.log("test");
+function add(n1, n2, showResult, phrase) {
+    // if(typeof n1 !== "number"){
+    //     throw new Error("it has to be a number")
+    // }
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+}
+var number1 = 5;
+var number2 = 7.8;
+var printResult = true;
+var resultPhrase = "The result is...";
+add(number1, number2, printResult, resultPhrase);
