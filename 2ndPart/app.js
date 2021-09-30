3741daf93d54d22d66ba88f8e41f521747a7f1e0
Laura <65158950+lauratro@users.noreply.github.com>
2021-09-30 11:36:06 +0200

    Tuple and Enum part

--- 2ndPart/app.js
+++ 2ndPart/app.js
@@ -15,3 +15,13 @@ var number2 = 7.8;
 var printResult = true;
 var resultPhrase = "The result is...";
 add(number1, number2, printResult, resultPhrase);
+// Ts and Arrays
+var person = {
+    name: "Laura",
+    age: 20,
+    hobbies: ['sports', "cooking"],
+    role: [2, "Author"]
+};
+person.role.push('admin'); //Typescript doesn't recognize this error
+person.role[1] = 10; // Error it should be a string
+var favoriteActivities;
--- 2ndPart/app.ts
+++ 2ndPart/app.ts
@@ -16,3 +16,26 @@ const number2 =7.8;
 const printResult= true
 const resultPhrase ="The result is..."
 add(number1, number2, printResult, resultPhrase)
+// Ts and Arrays
+const person:{
+    name:string;
+    age:number;
+    hobbies:string[];
+    role:[number, string] //tuple
+}={
+    name:"Laura",
+    age:20,
+    hobbies:['sports', "cooking"],
+    role:[2,"Author"]
+}
+person.role.push('admin'); //Typescript doesn't recognize this error
+//person.role[1] =10 // Error it should be a string
+let favoriteActivities: string[];
+//ENUM - costum type
+enum Role {ADMIN ='Admin', READ_ONLY=100, AUTHOR='Author' }; //ASSIGN LABEL TO NUMBER. I can assign a different number instead to start with 0, or he can be a string
+const person2={
+    name:"Laura",
+    age:20,
+    hobbies:['sports', "cooking"],
+    role: Role.ADMIN
+}
