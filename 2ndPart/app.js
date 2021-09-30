a14f1268184ca836ef64ac656b378b551288468a
Laura <65158950+lauratro@users.noreply.github.com>
2021-09-30 12:09:12 +0200

    Union, literal and alias type

--- 2ndPart/app.js
+++ 2ndPart/app.js
@@ -1,27 +1,15 @@
-function add(n1, n2, showResult, phrase) {
-    // if(typeof n1 !== "number"){
-    //     throw new Error("it has to be a number")
-    // }
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
+//UNION
+function combine(n1, n2) {
+    var result;
+    if (typeof n1 === 'number' && typeof n2 === 'number') {
+        result = n1 + n2; //TS see the union type but not what there is 
     }
     else {
-        return result;
+        result = n1.toString() + n2.toString();
     }
+    return result;
 }
-var number1 = 5;
-var number2 = 7.8;
-var printResult = true;
-var resultPhrase = "The result is...";
-add(number1, number2, printResult, resultPhrase);
-// Ts and Arrays
-var person = {
-    name: "Laura",
-    age: 20,
-    hobbies: ['sports', "cooking"],
-    role: [2, "Author"]
-};
-person.role.push('admin'); //Typescript doesn't recognize this error
-person.role[1] = 10; // Error it should be a string
-var favoriteActivities;
+var combineAges = combine(30, 26);
+console.log(combineAges);
+var combineNames = combine('Terry', 'Mary');
+console.log(combineNames);
---
+++ 2ndPart/union-literal-alias.ts
@@ -0,0 +1,28 @@
+//UNION and UNION TYPE COMBINE WITH LITERAL TYPE
+type Combinale = number | string; // I can store an union in the type
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+function combine(n1: Combinale, n2: Combinale, resultConversion: ConversionDescriptor ) {
+    let result;
+    if (typeof n1 === 'number' && typeof n2 === 'number'|| resultConversion ==="as-number") {
+        result = +n1 + +n2; //TS see the union type but not what there is 
+   
+     
+    } else {
+        result = n1.toString() + n2.toString();
+    }
+  /*   if (resultConversion == 'as-number') {
+        return +result
+    }
+    else {
+        return result.toString()
+    } */
+   return result;
+    
+}
+const combineAges = combine(30, 26, " as-number");
+console.log(combineAges)
+const combineStringAges = combine('30', '26', " as-number");
+console.log(combineStringAges)
+const combineNames = combine('Terry', 'Mary', 'as-age');
+console.log(combineNames)
